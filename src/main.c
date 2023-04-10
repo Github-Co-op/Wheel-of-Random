1e9cd62bd231dfde13679da2aededffe39b6218b
notmacos <111141400+notmacos@users.noreply.github.com>
2023-04-10 14:17:41 -0400

    test

--- src/main.c
+++ src/main.c
@@ -12,3 +12,22 @@ int main() {
     return 0;
 }
 
+// Spin() will randomly choose an option provided
+int spin() {
+
+    // Call variables
+    char choice1[25];
+    char choice2[25];
+
+    // Request choice
+    printf("Let's start deciding!\n");
+
+    printf("Enter choice one: ");
+    fgets(choice1, sizeof(choice1), stdin);
+
+    printf("Enter choice two: ");
+    fgets(choice2, sizeof(choice2), stdin);
+
+    printf("Choice one is %s and choice two is %s", choice1, choice2);
+    return 0;
+}
